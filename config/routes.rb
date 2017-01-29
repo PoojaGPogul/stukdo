8667bf442ba654498b97cdeccd42aac1812e62aa
YOUR NAME <YOUR@EMAIL.com>
2017-01-29 11:54:06 +0530

    task panel design done

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'about' => "pages#about"
+
+	get 'test' => "pages#test"
+
 end
